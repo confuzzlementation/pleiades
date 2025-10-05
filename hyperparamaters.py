0c75cc0f43050842842f8890248fe4e986f5e53e
ProperDiamond <theprogamergroup@gmail.com>
2025-10-05 16:56:10 +1100

    started fixing format of hyperparamater, bug (it dosent fit the whole page)

--- hyperparamaters.py
+++ hyperparamaters.py
@@ -83,7 +83,7 @@ def objective(trial):
 
     return sum(aucs) / len(aucs)
 
-study = optuna.create_study(direction="maximize", sampler=optuna.samplers.TPESampler(seed=123, multivariate=True, group=True), pruner=optuna.pruners.SuccessiveHalvingPruner(reduction_factor=3, min_resource=200))
+study = optuna.create_study(direction="maximize", sampler=optuna.samplers.TPESampler(seed=123, multivariate=True, group=True), pruner=optuna.pruners.SuccessiveHalvingPruner(reduction_factor=3, min_resource=100))
 study.optimize(objective, n_trials= 50, show_progress_bar=True)
 
 print("Best parameters:", study.best_params)
